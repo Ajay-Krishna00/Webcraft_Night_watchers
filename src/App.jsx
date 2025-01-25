4274e6300a8fc2db9af4dc567e27785596afab0d
Abhay Murali <abhaymuralim9@gmail.com>
2025-01-25 13:23:41 +0530

    game page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import GameCarousal from './components/gameCarousal';
+import GamePage from './GamePage';
 
 function App() {
   
 
   return (
     <>
-      <GameCarousal />
+      {/* <GameCarousal /> */}
+      <GamePage/>
     </>
   )
 }
